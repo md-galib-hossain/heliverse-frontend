8b90dc8105af79330b14d144fa701a2c323c097b
Md Galib Hossain <mdgalib23@gmail.com>
2024-08-27 17:26:10 +0600

    feat: intial setup nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    images : {
+        remotePatterns : [
+            {
+                protocol : "https",
+                hostname : "**"
+            }
+        ]
+    },
+    
+      eslint: {
+        ignoreDuringBuilds: false,
+      },
+}
 export default nextConfig;
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,6 +2,9 @@
 const config = {
   plugins: {
     tailwindcss: {},
+    autoprefixer: {},
+
+
   },
 };
 
